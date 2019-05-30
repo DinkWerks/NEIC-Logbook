93e0fa29fda672e9e6c085d4843e8be940b6ef13
Kyle Deutsch <kdeutsch@csuchico.edu>
2019-05-30 11:25:41 -0700

    Begin Filtering

--- mClientList/ViewModels/ClientListViewModel.cs
+++ mClientList/ViewModels/ClientListViewModel.cs
@@ -3,7 +3,9 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 using Tracker.Core.StaticTypes;
 
 namespace mClientList.ViewModels
@@ -11,6 +13,9 @@ namespace mClientList.ViewModels
     public class ClientListViewModel : BindableBase
     {
         private ObservableCollection<ClientEntryViewModel> _clientEntries = new ObservableCollection<ClientEntryViewModel>();
+        public CET = CollectionViewSource.GetDefaultView();
+        private string _clientNameSearchText;
+        private string _peidSearchText;
 
         public ObservableCollection<ClientEntryViewModel> ClientEntries
         {
@@ -18,15 +23,32 @@ namespace mClientList.ViewModels
             set { SetProperty(ref _clientEntries, value); }
         }
 
+        public string PEIDSearchText
+        {
+            get { return _peidSearchText; }
+            set { SetProperty(ref _peidSearchText, value); }
+        }
+
+        public string ClientNameSearchText
+        {
+            get { return _clientNameSearchText; }
+            set { SetProperty(ref _clientNameSearchText, value); }
+        }
+
         public ClientListViewModel()
         {
             AddTestClients();
         }
 
+        private void FilterClientNames(object obj)
+        {
+
+        }
+
         private void AddTestClients()
         {
-            for (int i=0; i<11; i++)
-            ClientEntries.Add(new ClientEntryViewModel());
+            for (int i = 0; i < 11; i++)
+                ClientEntries.Add(new ClientEntryViewModel());
         }
     }
 }
